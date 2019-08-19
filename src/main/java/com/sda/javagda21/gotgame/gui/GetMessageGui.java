2ad6382ff7ad114d1fcb5fc65f769f01bdd9241f
Kasia <katzaw89@wp.pl>
2019-08-19 22:02:03 +0200

    new changes

---
+++ src/main/java/com/sda/javagda21/gotgame/gui/GetMessageGui.java
@@ -0,0 +1,44 @@
+package com.sda.javagda21.gotgame.gui;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.DocumentSnapshot;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.FirestoreException;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.FirestoreClient;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Nullable;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@Component
+public class GetMessageGui {
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void main() throws IOException {
+        FileInputStream serviceAccount =
+                new FileInputStream("C:\\Users\\kzawi\\IdeaProjects\\gotgame\\src\\main\\resources\\static\\friendlychat-b2806-firebase-adminsdk-zttwa-a318ab83ec.json");
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .setDatabaseUrl("https://friendlychat.firebaseio.com")
+                .build();
+
+
+        FirebaseApp.initializeApp(options);
+        Firestore db = FirestoreClient.getFirestore();
+
+
+        DocumentReference docRef = db.collection("messages").document("message");
+        docRef.addSnapshotListener((documentSnapshot, e) -> {
+            assert documentSnapshot != null;
+            System.out.println("Current data: " + documentSnapshot.getData());
+        });
+
+
+    }
+}
--- src/main/java/com/sda/javagda21/gotgame/model/MessageType.java
+++
@@ -1,7 +0,0 @@
-package com.sda.javagda21.gotgame.model;
-
-public enum MessageType {
-
-    CHAT,JOIN,LEAVE
-
-}
