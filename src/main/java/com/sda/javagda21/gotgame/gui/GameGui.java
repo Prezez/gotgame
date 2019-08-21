40f75df8c6c1ed48573260490802ef14701f3180
DamianRybka <damian.rybka@op.pl>
2019-08-21 20:37:15 +0200

    First app look

--- src/main/java/com/sda/javagda21/gotgame/gui/GameGui.java
+++ src/main/java/com/sda/javagda21/gotgame/gui/GameGui.java
@@ -6,19 +6,18 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
 @Route("game")
-public class GameGui extends ClassLoader {
-    private ClassLoader targetLoader;
-    Map map = Map.createNewMap();
-
-    public GameGui(ClassLoader p) {
-        this.targetLoader = Thread.currentThread().getContextClassLoader();
-        if (this.targetLoader==null) {
-            throw new IllegalStateException("Cannot set up custom class loader: No context class loader set");
-        }
-    }
-
-
-    }
+public class GameGui extends VerticalLayout {
+//    private ClassLoader targetLoader;
+//
+//    public GameGui(ClassLoader p) {
+//        this.targetLoader = Thread.currentThread().getContextClassLoader();
+//        if (this.targetLoader==null) {
+//            throw new IllegalStateException("Cannot set up custom class loader: No context class loader set");
+//        }
+//    }
+//
+//
+}
 
 
 
