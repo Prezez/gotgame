f9795e458704c03aaec117ffdd4a93ea5edd201d
Kasia <katzaw89@wp.pl>
2019-08-28 19:46:45 +0200

    change security

--- src/main/java/com/sda/javagda21/gotgame/config/WebSecurityConfig.java
+++ src/main/java/com/sda/javagda21/gotgame/config/WebSecurityConfig.java
@@ -2,13 +2,6 @@ package com.sda.javagda21.gotgame.config;
 
 import com.sda.javagda21.gotgame.repository.AppUserRepo;
 import com.sda.javagda21.gotgame.service.UserDetailsServiceImpl;
-import com.vaadin.flow.component.ClickEvent;
-import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.html.Image;
-import com.vaadin.flow.component.html.Label;
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.textfield.TextField;
-import com.vaadin.flow.router.Route;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Bean;
@@ -17,10 +10,8 @@ import org.springframework.context.event.EventListener;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import com.sda.javagda21.gotgame.config.*;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -43,11 +34,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/test1").permitAll()
-//                .antMatchers("/test2").hasRole("USER")
-//                .antMatchers("/test3").hasRole("ADMIN")
-//                .and()
-//                .formLogin().permitAll()
-                .and().csrf().disable();
+                .antMatchers("/test2").hasRole("USER")
+                .antMatchers("/test3").hasRole("ADMIN")
+                .antMatchers("/chatAll").permitAll()
+                .antMatchers("/sendMessage").permitAll()
+                .antMatchers("/chatRegister").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().permitAll()
+                .and()
+                .csrf()
+                .disable();
     }
 
     @Bean
