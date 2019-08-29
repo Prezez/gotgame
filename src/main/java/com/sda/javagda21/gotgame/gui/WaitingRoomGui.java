002d57977653c5504006a934325dbcd46f51589e
Prezez <47228750+Prezez@users.noreply.github.com>
2019-08-29 23:06:50 +0200

    waiting room page completed

--- src/main/java/com/sda/javagda21/gotgame/gui/WaitingRoomGui.java
+++ src/main/java/com/sda/javagda21/gotgame/gui/WaitingRoomGui.java
@@ -2,24 +2,17 @@ package com.sda.javagda21.gotgame.gui;
 
 
 import com.sda.javagda21.gotgame.config.AppUser;
-import com.sda.javagda21.gotgame.repository.AppUserRepo;
-import com.sda.javagda21.gotgame.repository.PlayerRepository;
-import com.sda.javagda21.gotgame.service.PlayerService;
-import com.vaadin.flow.component.Composite;
+import com.sda.javagda21.gotgame.service.AppUserService;
 import com.vaadin.flow.component.UI;
-import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.VaadinSession;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Stream;
-
 
 @Route("waiting-room")
 @StyleSheet("../frontend/styles/stylesVd.css")
@@ -27,8 +20,7 @@ public class WaitingRoomGui extends VerticalLayout {
 
     private AppUser appUser;
 
-
-    public WaitingRoomGui() {
+    public WaitingRoomGui(AppUserService appUserService) throws InterruptedException {
 
         Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         String username;
@@ -39,14 +31,35 @@ public class WaitingRoomGui extends VerticalLayout {
         }
 
 
+        List<AppUser> users = appUserService.findAll();
+        System.out.println("Liczba" + users.size());
+
+
+
+        do {
+            if (users.size() <2){
+//                Thread.sleep(5000);
+                UI.getCurrent().getPage().reload();}
+            else {
+                String playerOne = users.get(0).getUsername();
+                String playerTwo = users.get(1).getUsername();
+                VaadinSession.getCurrent().getSession().setAttribute("playerOne", playerOne);
+                VaadinSession.getCurrent().getSession().setAttribute("playerTwo", playerTwo);
+                UI.getCurrent().navigate("game");
+                UI.getCurrent().getPage().reload();
+            }
+        } while (users.size() < 2);
+
+
+
         Label namePlayer1 = new Label(username);
         Label namePlayer2 = new Label("Waiting for second Player: ");
-        TextField textPlayer2 = new TextField();
+//        TextField textPlayer2 = new TextField();
 
 
-        Button button = new Button("Start Game");
-        Button button2 = new Button("Refresh");
-        button2.addClickListener(clickEvent -> UI.getCurrent().getPage().reload());
+//        Button button = new Button("Start Game");
+//        Button button2 = new Button("Refresh");
+//        button2.addClickListener(clickEvent -> UI.getCurrent().getPage().reload());
 
 //            button.addClickListener(e -> {
 //                if (!namePlayer1.getValue().equals("") && !textPlayer2.getValue().equals("")) {
@@ -55,8 +68,9 @@ public class WaitingRoomGui extends VerticalLayout {
 //            });
 
 
-        add(namePlayer1, namePlayer2, textPlayer2, button2, button);
+        add(namePlayer1, namePlayer2);
 
 
     }
 }
+
--- src/main/java/com/sda/javagda21/gotgame/repository/AppUserRepo.java
+++ src/main/java/com/sda/javagda21/gotgame/repository/AppUserRepo.java
@@ -7,6 +7,4 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface AppUserRepo extends JpaRepository<AppUser, Long> {
     AppUser findByUsername(String username);
-    AppUser findById(long id);
-    AppUser findAllBy(String username);
 }
