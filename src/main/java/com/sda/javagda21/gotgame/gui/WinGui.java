8a33e96bae9f3dcd61a5a78f85123b6014a09361
Prezez <47228750+Prezez@users.noreply.github.com>
2019-08-31 12:15:01 +0200

    New database added

--- src/main/java/com/sda/javagda21/gotgame/gui/WinGui.java
+++ src/main/java/com/sda/javagda21/gotgame/gui/WinGui.java
@@ -1,16 +1,30 @@
 package com.sda.javagda21.gotgame.gui;
 
+import com.sda.javagda21.gotgame.config.AppUser;
+import com.sda.javagda21.gotgame.repository.AppUserRepo;
+import com.sda.javagda21.gotgame.service.AppUserService;
 import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.router.Route;
+
+import java.util.List;
 
 @Route("win")
 public class WinGui extends HorizontalLayout {
 
 
-    public WinGui(){
+    public WinGui(AppUserService appUserService, AppUserRepo appUserRepo){
         Label label = new Label();
         label.setText("Wygrałeś!!");
         add(label);
+
+        List<AppUser> activeUsers = appUserService.findAllByActive(true);
+
+        for (AppUser user : activeUsers) {
+            AppUser inactiveUser = user;
+            inactiveUser.setActive(false);
+            appUserRepo.save(inactiveUser);
+        }
+
     }
 }
