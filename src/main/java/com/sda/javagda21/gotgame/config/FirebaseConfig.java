9c0dc94ef9c3a0d3e26e91f6458df7975b305d57
Prezez <47228750+Prezez@users.noreply.github.com>
2019-08-31 11:17:01 +0200

    Firebase path adjusted

--- src/main/java/com/sda/javagda21/gotgame/config/FirebaseConfig.java
+++ src/main/java/com/sda/javagda21/gotgame/config/FirebaseConfig.java
@@ -7,19 +7,17 @@ import org.springframework.context.annotation.Configuration;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
 
     public FirebaseConfig() throws IOException {
-        FileInputStream serviceAccount =
-                new FileInputStream("classpath:static\\friendlychat-b2806-firebase-adminsdk-zttwa-a318ab83ec.json");
+        InputStream resourceAsStream = this.getClass().getResourceAsStream("/firebase/friendlychat-b2806-firebase-adminsdk-zttwa-a318ab83ec.json");
         FirebaseOptions options = new FirebaseOptions.Builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .setCredentials(GoogleCredentials.fromStream(resourceAsStream))
                 .setDatabaseUrl("https://friendlychat.firebaseio.com")
                 .build();
-
-
         FirebaseApp.initializeApp(options);
     }
 }
